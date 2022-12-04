681cd67bc9a0b84895e70d9c5c410ea4b699b076
lucyferrior <sefa404e@gmail.com>
2022-12-04 17:56:08 +0300

    index sayfası ve ürünler sayfası eklendi

---
+++ pages/Products.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div id="product_container">
+            <Product v-for="item in 3"/>
+        </div>
+    </div>
+</template>
+<style>
+#product_container {
+    display: flex;
+    row-gap: 0;
+    overflow: hidden;
+    flex-flow: row wrap;
+}
+</style>
+<script>
+export default {
+    name: "product-vue"
+}
+</script>
