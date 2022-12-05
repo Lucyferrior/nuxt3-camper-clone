199a5e6b7f90d4ebe8af57f30cfd3546c7605a19
lucyferrior <sefa404e@gmail.com>
2022-12-05 22:08:45 +0300

    product detail ve fotolar eklendi

---
+++ pages/Product-Details.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="div-row">
+        <div class="div_column images_column">
+            <div class="div-row" style="row-gap: 0px;">
+                <ProductDetailPhoto v-for="item in 5"/>
+            </div>
+        </div>
+        
+    </div>  
+</template>
+<style scoped>
+.div_column {
+    min-height: 1px;
+    position: relative;
+    max-width: 100%;
+}
+
+.images_column {
+    display: flex;
+    flex: 0 0 100%;
+    max-width: 100%;
+}
+
+.div-row {
+    display: flex;
+    flex-flow: row wrap;
+}
+.product_info_style>div {
+    height: 100%;
+}
+.product_info_style {
+    display: block;
+    flex: 0 0 100%;
+    max-width: 100%;
+}
+
+@media screen and (min-width: 768px) {
+    .images_column {
+        flex: 0 0 58.333333%;
+        max-width: 58.333333%;
+    }
+    .product_info_style {
+        flex: 0 0 41.6666667%;
+        max-width: 41.6666667%;
+    }
+}
+
+@media screen and (min-width: 992px) {
+    .images_column {
+        flex: 0 0 66.66666667%;
+        max-width: 66.6666667%;
+    }
+    .product_info_style {
+        flex: 0 0 33.3333333%;
+        max-width: 33.33333333%;
+    }
+}
+@media screen and (min-width: 768px) {
+    .product_info_style {
+        padding: 35px 0;
+        margin: 0 auto;
+        max-width: 260px;
+    }
+}
+</style>
