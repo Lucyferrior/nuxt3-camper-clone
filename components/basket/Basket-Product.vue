ca853b0d01c4ecc746c320c500846b0aae2ab3a8
lucyferrior <sefa404e@gmail.com>
2022-12-07 17:35:42 +0300

    basket:sepet oluşturuldu

---
+++ components/basket/Basket-Product.vue
@@ -0,0 +1,181 @@
+<template>
+    <div class="shopping_bag_row_item" style="row-gap: 0px;">
+        <div class="ant-col">
+            <a href="https://www.camper.com/tr_TR/women/shoes/twins/camper-twins-K201311-024">
+                <picture>
+                    <img src="https://cloud.camper.com/is/image/JGVzaG9wMDNiYWdncmV5JA==/K201311-024_L.jpg"
+                        alt="Image of thumbnail image 0 of K201311-024" class="item-thumbnail">
+                </picture>
+            </a>
+        </div>
+        <div class="ürün-genel">
+            <div class="yazı-genel">
+                <p class="name">Twins</p>
+                <p class="kod">K201311-024</p>
+                <div class="bilgi">
+                    <div>
+                        <span class="size">Size</span>
+                        <button type="button" class="beden"><span>40</span>
+                            <svg height="6" viewBox="0 0 18 9" width="11">
+                                <path d="m4-4 3.67495264 3.67495264 4.32504736 4.32504736-8 8" fill="none" stroke="#000"
+                                    stroke-linecap="square" transform="matrix(0 1 -1 0 12.96 -3.141463)"></path>
+                            </svg>
+                        </button>
+                    </div>
+                    <span class="price">
+                        <span data-ge-basket-productsaleprice="data-ge-basket-productsaleprice">₺2.699,00
+                        </span>
+                    </span>
+                </div>
+            </div>
+        </div>
+        <div class="carpi-icon">
+            <div>
+                <span class="carpi">
+                    <svg height="12" viewBox="0 0 10 12" width="10">
+                        <path d="m9.79049 9.789734-9.79049-9.78965m9.79049-.000084-9.79049 9.78965" fill="none"
+                            stroke="#000" transform="translate(0 1)"></path>
+                    </svg>
+                </span>
+            </div>
+        </div>
+    </div>
+</template>
+<style>
+
+.shopping_bag_row_item {
+    row-gap: 0px;
+    border-bottom: 1px solid #d9d9d9;
+    padding: 10px 0;
+    flex-flow: nowrap;
+    min-height: 141px;
+    display: flex;
+}
+
+@media screen and (min-width: 768px) {
+    .ürün-genel {
+        display: flex;
+        justify-content: flex-start;
+    }
+}
+
+.ürün-genel {
+    flex: auto;
+    position: relative;
+    max-width: 100%;
+    min-height: 1px;
+}
+
+.ant-col {
+    flex: 0 0 120px;
+    position: relative;
+    max-width: 100%;
+    min-height: 1px;
+}
+
+
+.item-thumbnail {
+    max-width: 100%;
+    width: 120px;
+    height: 120px;
+    object-fit: cover;
+    object-position: bottom;
+    vertical-align: middle;
+    border-style: none;
+
+}
+
+.kod {
+    font-size: .6em;
+    color: gray;
+    margin-bottom: 0;
+}
+
+.name {
+    font-size: var(--title-size);
+    margin-bottom: 0;
+}
+
+.yazı-genel {
+    margin-left: 1em;
+}
+
+.size {
+    font-size: var(--description-size);
+}
+
+.bilgi {
+    margin-top: 1em;
+    text-transform: uppercase;
+}
+
+.beden>svg:not(:root) {
+    overflow: hidden;
+}
+
+.beden>svg {
+    margin-left: 8px;
+}
+
+.beden>span {
+    font-size: var(--description-size);
+    display: inline-block;
+}
+
+.beden,
+input {
+    overflow: visible;
+}
+
+.beden,
+select {
+    text-transform: none;
+}
+
+.beden {
+    padding: 0 5px;
+    margin-right: 2em;
+    color: black;
+    background: transparent;
+    border-color: transparent;
+    box-shadow: none;
+    height: 24px;
+    font-size: 16px;
+    line-height: 1.5715;
+    position: relative;
+    display: inline-block;
+    font-weight: 400;
+    white-space: nowrap;
+    text-align: center;
+    cursor: pointer;
+    transition: all .3s cubic-bezier(.645, .045, .355, 1);
+    user-select: none;
+    touch-action: manipulation;
+    color: black;
+    border: 1px solid #d9d9d9;
+}
+
+.price {
+    display: inline-block;
+    font-size: var(--title-size);
+}
+
+.carpi>svg:not(:root) {
+    overflow: hidden;
+}
+
+.carpi {
+    color: var(--color-text-secondary);
+    cursor: pointer;
+}
+
+.carpi-icon {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-left: 1rem;
+    position: relative;
+    max-width: 100%;
+    min-height: 1px;
+}
+</style>
---
+++ pages/Basket.vue
@@ -0,0 +1,103 @@
+<template>
+    <div class="shopping_bag_wrapper">
+        <div>
+            <h2 class="bag_title">Alisveris SEPETİNİZDE</h2>
+            <div class="bag_subtitle">
+                <p>Sepetinizde 2 ürünler var.</p>
+                <a>Alışverişe devam et
+                    <svg viewBox="0 0 17 17" class="shopping-icon">
+                        <g fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"
+                            transform="translate(1 1)">
+                            <path d="m7.499631 12v-9" stroke="#000"></path>
+                            <path d="m11.99941 7.5h-8.999558" stroke="#000"></path>
+                            <path
+                                d="m14.9992622 7.5c0 4.1421545-3.3578456 7.5-7.50000003 7.5-4.14141662 0-7.49926217-3.3578455-7.49926217-7.5 0-4.14215445 3.35784555-7.5 7.49926217-7.5 4.14215443 0 7.50000003 3.35784555 7.50000003 7.5z"
+                                stroke="#1d1d1b"></path>
+                        </g>
+                    </svg>
+                </a>
+            </div>
+            <div class="loading">
+                <div class="container">
+                    <BasketProduct />
+                    <BasketProduct />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+.shopping_bag_wrapper {
+    margin: 28px auto 0;
+    max-width: 75%;
+}
+
+.shopping_bag_wrapper {
+    grid-template-columns: 785px 300px;
+    padding: 0 24px;
+    grid-gap: 74px;
+    display: grid;
+}
+
+.container {
+    position: relative;
+    transition: opacity .3s;
+}
+
+.loading {
+    position: relative;
+}
+
+.gclass {
+    fill: none;
+    fill-rule: evenodd;
+    stroke-linecap: round;
+    stroke-linejoin: round;
+    transform: translate(1, 1);
+}
+
+.shopping-icon {
+    margin-left: 5px;
+    height: 17px;
+    width: 17px;
+
+}
+
+.shopping-icon:not(:root) {
+
+    overflow: hidden;
+}
+
+.bag_title {
+    font-size: 1.5em;
+    font-weight: 400;
+}
+
+.bag_subtitle {
+    text-transform: uppercase;
+    font-size: 10px;
+    border-bottom: 1px solid #d9d9d9;
+    line-height: 1em;
+    text-anchor: middle;
+    align-items: center;
+    display: flex;
+    justify-content: space-between;
+}
+
+.bag_subtitle p {
+    padding-top: 9px;
+    align-items: center;
+    display: flex;
+}
+
+.bag_subtitle a {
+    align-items: center;
+    display: flex;
+}
+</style>
+<style scoped>
+p {
+    margin-bottom: 1em;
+}
+</style>
